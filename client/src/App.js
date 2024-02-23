e277e6e5046740d793a91abd68b45e300a513a45
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-02-23 16:31:33 +0500

    Add React Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import "./App.css";
-import Layout from "./components/layout/Layout";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import Aboutus from "./pages/Aboutus";
+import Contactus from "./pages/Contactus";
+import Sign from "./pages/Sign";
+import Signup from "./pages/Signup";
+import PageNotFound from "./pages/PageNotFound";
+import Policy from "./pages/Policy";
 
 function App() {
   return (
     <>
-      <Layout>
-        <h2>E-Commerece</h2>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/aboutUs" element={<Aboutus />} />
+        <Route path="/contactUs" element={<Contactus />} />
+        <Route path="/signIn" element={<Sign />} />
+        <Route path="/signUp" element={<Signup />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
