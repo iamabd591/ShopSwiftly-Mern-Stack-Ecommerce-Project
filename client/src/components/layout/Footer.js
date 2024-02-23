91ac94e49d1eb8357bb7562829318d39bf05d22f
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-02-23 15:58:04 +0500

    basic footer design

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,10 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      <section>
-        <h3>ShopSwiftyl</h3>
-      </section>
+    <div className='bg-dark text-light p-3'>
+     <h4 className='text-center'>All Right Reserved &copy; Shop Swiftly</h4>
     </div>
   )
 }
