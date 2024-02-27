36ccceb6491a9dbb3c9ae020edce0fffa1fdd79c
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-02-27 19:00:11 +0500

    404 page design

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -5,15 +5,17 @@ import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return (
     <Layout>
-      <div className="d-flex align-items-center justify-content-center vh-100">
+      <div className="d-flex align-items-center justify-content-center vh-100 notfound">
         <div className="text-center">
-          <h1 className="display-1 fw-bold">404</h1>
-          <p className="fs-3">
+          <h1 className="display-1 fw-bold text-404">404</h1>
+          <p className="fs-3 p-text">
             {" "}
-            <span className="text-danger">Opps!</span> Page not found.
+            <span className="text-danger danger">Opps!</span> Page not found.
           </p>
-          <p className="lead">The page you’re looking for doesn’t exist.</p>
-          <Link to="/" className="btn btn-primary">
+          <p className="lead lead-txt">
+            The page you’re looking for doesn’t exist.
+          </p>
+          <Link to="/" className="btn btn-primary btn-home">
             Go Home
           </Link>
         </div>
