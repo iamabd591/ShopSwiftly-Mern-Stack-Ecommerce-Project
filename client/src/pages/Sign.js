52dd197be2b09a41593af0e8bdde2bc722bd93a6
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-03-04 22:41:21 +0500

    style sign in page

--- client/src/pages/Sign.js
+++ client/src/pages/Sign.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
 import { TiShoppingCart } from "react-icons/ti";
+import { Link } from "react-router-dom";
 
 const Sign = () => {
   return (
@@ -9,7 +10,7 @@ const Sign = () => {
         <div className="form-div">
           <form method="post" action="#">
             <h2>Log In </h2>
-            <p>
+            <p className="slogan">
               And Enjoy Happy Shopping{" "}
               <spam>
                 {" "}
@@ -36,14 +37,33 @@ const Sign = () => {
               />
             </div>
 
+            <div className="form-links">
             <div className="input-check">
               <input type="checkbox" id="remember" />
               <lable htmlFor="remember">Remember Me</lable>
             </div>
-
-            <div className="logi-btn">
-              <button> Login </button>
+              <div className="fogot-pass">
+                <Link to="/forgot" tabIndex="-1">
+                  {" "}
+                  Forgot Password ?
+                </Link>
+              </div>
             </div>
+
+            <div className="login-btn">
+              <button> Log in </button>
+            </div>
+            <div className="create-account">
+                <p>
+                  Don't Have an account{" "}
+                  <spam>
+                    {" "}
+                    <Link to="/signup" tabIndex="-1">
+                      Click Here
+                    </Link>
+                  </spam>
+                </p>
+              </div>
           </form>
         </div>
       </div>
