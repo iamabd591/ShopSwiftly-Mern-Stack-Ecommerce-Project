e31fa7fc31001a311e15c6b5e42f096638fa6de4
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-03-04 21:37:51 +0500

    login form

--- client/src/pages/Sign.js
+++ client/src/pages/Sign.js
@@ -1,12 +1,54 @@
-import React from 'react'
-import Layout from '../components/layout/Layout'
+import React from "react";
+import Layout from "../components/layout/Layout";
+import { TiShoppingCart } from "react-icons/ti";
 
 const Sign = () => {
   return (
     <Layout>
-        <h1>Sign in Page</h1>
-    </Layout>
-  )
-}
+      <div className="main">
+        <div className="form-div">
+          <form method="post" action="#">
+            <h2>Log In </h2>
+            <p>
+              And Enjoy Happy Shopping{" "}
+              <spam>
+                {" "}
+                <TiShoppingCart />
+              </spam>
+            </p>
+            <div className="input-field">
+              <input
+                type="email"
+                placeholder="Enter Your Email"
+                id="email"
+                required
+                autoComplete="off"
+              />
+            </div>
 
-export default Sign
+            <div className="input-field">
+              <input
+                type="password"
+                placeholder="Enter Your Password"
+                id="password"
+                required
+                autoComplete="off"
+              />
+            </div>
+
+            <div className="input-check">
+              <input type="checkbox" id="remember" />
+              <lable htmlFor="remember">Remember Me</lable>
+            </div>
+
+            <div className="logi-btn">
+              <button> Login </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Sign;
