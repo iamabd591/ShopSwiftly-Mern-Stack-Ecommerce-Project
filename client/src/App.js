bf2735f90dc51dde4c40f5fb3cdb74438ec47139
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-03-05 22:29:25 +0500

    redesign signup page

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,7 @@ import HomePage from "./pages/Home";
 import Aboutus from "./pages/Aboutus";
 import Contactus from "./pages/Contactus";
 import Sign from "./pages/Sign";
-import PersonalInfo from "./pages/SignUp";
-import ResidtionalInfo from "./pages/Signup_step_2";
-import BankInfo from "./pages/Signup_step_3";
+import SignUp from "./pages/SignUp";
 import Policy from "./pages/Policy";
 import Feadback from "./pages/feadback";
 import PageNotFound from "./pages/PageNotFound";
@@ -18,9 +16,7 @@ function App() {
         <Route path="/aboutUs" element={<Aboutus />} />
         <Route path="/contactUs" element={<Contactus />} />
         <Route path="/signIn" element={<Sign />} />
-        <Route path="/SignUp" element={<PersonalInfo />} />
-        <Route path="/signup_step_2" element={<ResidtionalInfo />} />
-        <Route path="/signup_step_3" element={<BankInfo />} />
+        <Route path="/SignUp" element={<SignUp />} />
         <Route path="/policy" element={<Policy />} />
         <Route path="/feadback" element={<Feadback />} />
         <Route path="*" element={<PageNotFound />} />
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -5,132 +5,76 @@ import Layout from "../components/layout/Layout";
 function Signup() {
   return (
     <Layout>
-      <div className="register-form">
-        <div className="form-image">
-          {/* <img
-            src="../images/Registration.jpg"
-            alt="Description of the image"
-          /> */}
-        </div>
-
-        <div className="input-form">
-          <form className="form-register" action="#" method="post">
-            <h3 className="heading">Personal Information</h3>
-            <p>Please Enter your information so we can build your account.</p>
+      <div className="signup-main">
+        <div className="personal-info">
+          <h1>Personal Information</h1>
+          <p>Enter Your Personal Information</p>
+          <form method="post" action="#">
             <div className="name">
-              <fieldset>
-                <legend>
-                  First Name <span> * </span>
-                </legend>
+              <div className="first-name">
+                <lable>
+                  First Name<spna>*</spna>
+                </lable>
                 <input
                   type="text"
-                  className=" form-control"
-                  id="first-name"
-                  name="first-name"
                   placeholder="First Name"
                   required
+                  autoCapitalize="off"
                 />
-              </fieldset>
-           </div>
-
-            <div className="form-email">
-              <fieldset>
-                <legend>
-                  Email <span> * </span>
-                </legend>
+              </div>
+              <div className="last-name">
+                <lable>
+                  Last Name<spna>*</spna>
+                </lable>
                 <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  name="email"
-                  placeholder="Email"
+                  type="text"
+                  placeholder="Last Name"
                   required
+                  autoCapitalize="off"
                 />
-              </fieldset>
+              </div>
             </div>
-
-            <div className="form-password">
-              <fieldset>
-                <legend>
-                  Password <span> * </span>
-                </legend>
+            <div className="email">
+              <lable>
+                Email<spna>*</spna>
+              </lable>
+              <input
+                type="email"
+                placeholder="Enter Your Email"
+                required
+                autoComplete="off"
+              />
+            </div>
+            <div className="password-field">
+              <div className="Password">
+                <lable>
+                  Password<spna>*</spna>
+                </lable>
                 <input
                   type="password"
-                  className="form-control"
-                  id="password"
-                  name="password"
                   placeholder="Password"
                   required
+                  autoCapitalize="off"
                 />
-              </fieldset>
-
-              <fieldset>
-                <legend>
-                  Confrim Password <span> * </span>
-                </legend>
+              </div>
+              <div className="Cpassword">
+                <lable>
+                  Confrim Password<spna>*</spna>
+                </lable>
                 <input
                   type="password"
-                  className="form-control"
-                  id="cpassword"
-                  name="cpassword"
-                  placeholder=" Confrim Password"
+                  placeholder="Confrim Password"
                   required
+                  autoCapitalize="off"
                 />
-              </fieldset>
+              </div>
             </div>
-
-            <div className="form-gender">
-              <fieldset>
-                <legend>
-                  Gender <span> * </span>
-                </legend>
-                <div className="form-check">
-                  <input
-                    type="radio"
-                    id="male"
-                    name="gender"
-                    value="male"
-                    className="form-check-input"
-                  />
-                  <label htmlFor="male" className="form-check-label">
-                    Male
-                  </label>
-                </div>
-                <div className="form-check">
-                  <input
-                    type="radio"
-                    id="female"
-                    name="gender"
-                    value="female"
-                    className="form-check-input"
-                  />
-                  <label htmlFor="female" className="form-check-label">
-                    Female
-                  </label>
-                </div>
-              </fieldset>
-            </div>
-
-            {/* <div className="form-condition">
-              <fieldset>
-                <div className="form-check">
-                  <input
-                    type="checkbox"
-                    id="remember-me"
-                    name="remember-me"
-                    className="form-check-input"
-                  />
-                  <label htmlFor="remember-me" className="form-check-label">
-                    I Agreed With Terms & Conditions <span> * </span>
-                  </label>
-                </div>
-              </fieldset>
-            </div> */}
-
-            <div className="form-btn">
-              <NavLink to="./Signup_step_2" className="btn-submit">
-                Next Step
-              </NavLink>
+            <div className="gender">
+              <label>Gender:</label>
+              <input type="radio" id="male" name="gender" value="male" />
+              <label htmlFor="male">Male</label>
+              <input type="radio" id="female" name="gender" value="female" />
+              <label htmlFor="female">Female</label>
             </div>
           </form>
         </div>
