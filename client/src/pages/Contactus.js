b0f803b6ec1b075bb2ff59ae468caf18ebfbf7db
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-03-05 13:55:13 +0500

    add contact us page

--- client/src/pages/Contactus.js
+++ client/src/pages/Contactus.js
@@ -1,12 +1,61 @@
-import React from 'react'
-import Layout from '../components/layout/Layout'
-
+import React from "react";
+import Layout from "../components/layout/Layout";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaMobileAlt, FaClock } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 const Contactus = () => {
   return (
     <Layout>
-        <h1>Contact Us</h1>
-    </Layout>
-  )
-}
+      <div className="contact-main">
+        <div className="map"></div>
+        <div className="contact-info">
+          <h2>Get In Touch With us</h2>
 
-export default Contactus
+          <div className="contact-line">
+            <p>
+              <span>
+                <FaClock />
+              </span>
+              Time: 24/7 a Week
+            </p>
+          </div>
+
+          <div className="contact-line">
+            <p>
+              <span>
+                {" "}
+                <MdEmail />
+              </span>
+              Email: Info@ShopSwifly.com
+            </p>
+          </div>
+
+          <div className="contact-line">
+            <p>
+              <span>
+                {" "}
+                <FaMobileAlt />
+              </span>
+              Phone: +241 789256 3773
+            </p>
+          </div>
+
+          <div className="contact-line">
+            <p>
+              <span>
+                <FaLocationDot />
+              </span>
+              Address: 15205 North Kierland Blvd. Suite 100
+            </p>
+          </div>
+          <div className="contact-bttn">
+            <button className="login-register-btn">Login</button>
+            <button className="login-register-btn">Register</button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Contactus;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,22 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 
-const Signup = () => {
+function Signup() {
   return (
     <Layout>
       <div className="register-form">
-        {/* <div className="form-image">
-          <img
+        <div className="form-image">
+          {/* <img
             src="../images/Registration.jpg"
             alt="Description of the image"
-          />
-        </div> */}
+          /> */}
+        </div>
 
         <div className="input-form">
           <form className="form-register" action="#" method="post">
             <h3 className="heading">Personal Information</h3>
             <p>Please Enter your information so we can build your account.</p>
-            <div className="first-name">
+            <div className="name">
               <fieldset>
                 <legend>
                   First Name <span> * </span>
@@ -30,23 +31,7 @@ const Signup = () => {
                   required
                 />
               </fieldset>
-            </div>
-
-            <div className="last-name">
-              <fieldset>
-                <legend>
-                  Last Name <span> * </span>
-                </legend>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="last-name"
-                  name="last-name"
-                  placeholder="Last Name"
-                  required
-                />
-              </fieldset>
-            </div>
+           </div>
 
             <div className="form-email">
               <fieldset>
@@ -94,38 +79,6 @@ const Signup = () => {
               </fieldset>
             </div>
 
-            <div className="form-address">
-              <fieldset>
-                <legend>
-                  Address <span> * </span>
-                </legend>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="address"
-                  name="address"
-                  placeholder="Address"
-                  required
-                />
-              </fieldset>
-            </div>
-
-            <div className="form-phone">
-              <fieldset>
-                <legend>
-                  Phone <span> * </span>
-                </legend>
-                <input
-                  type="tel"
-                  className="form-control"
-                  id="phone"
-                  name="phone"
-                  placeholder="Phone"
-                  required
-                />
-              </fieldset>
-            </div>
-
             <div className="form-gender">
               <fieldset>
                 <legend>
@@ -158,23 +111,7 @@ const Signup = () => {
               </fieldset>
             </div>
 
-            <div className="form-ssn">
-              <fieldset>
-                <legend>
-                  Social Scurity No <span> * </span>
-                </legend>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="SSN"
-                  name="SSN"
-                  placeholder="SSN\CNIC\Passport No"
-                  required
-                />
-              </fieldset>
-            </div>
-
-            <div className="form-condition">
+            {/* <div className="form-condition">
               <fieldset>
                 <div className="form-check">
                   <input
@@ -188,16 +125,18 @@ const Signup = () => {
                   </label>
                 </div>
               </fieldset>
+            </div> */}
 
-              <div className="form-btn">
-                <button className="btn-submit">Create Account</button>
-              </div>
+            <div className="form-btn">
+              <NavLink to="./Signup_step_2" className="btn-submit">
+                Next Step
+              </NavLink>
             </div>
           </form>
         </div>
       </div>
     </Layout>
   );
-};
+}
 
 export default Signup;
