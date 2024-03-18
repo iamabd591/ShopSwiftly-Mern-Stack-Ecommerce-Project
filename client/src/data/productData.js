13d60c291ed0f16ab1963c82c5d540c1ad302541
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-03-18 12:54:34 +0500

    optimize home page

---
+++ client/src/data/productData.js
@@ -0,0 +1,58 @@
+export const dataList=[
+    {
+        id: 1,
+        title:'Chuck Taylor All Star Leater',
+        image:'https://img.freepik.com/free-vector/headphones-wireless-realistic-composition-with-isolated-image-phones-with-power-bank-dock-station-with-reflections-vector-illustration_1284-73201.jpg?w=740&t=st=1710748073~exp=1710748673~hmac=d05482852a13d814ad64b9ebf9f091b42ac7a66d30f336b8988daba204c14153',
+        price:50,
+        stock:true
+    },
+    {
+        id: 2,
+        title:'Chuck Taylor All Star Leater',
+        image:'https://www.freepik.com/free-vector/shopping-bags-mockup-realistic_6086083.htm#fromView=search&page=1&position=23&uuid=950d5669-c190-46d8-8b15-ac76797f45ff',
+        price:50,
+        stock:true
+    },
+    {
+        id: 3,
+        title:'Chuck Taylor All Star Leater',
+        image:'https://www.freepik.com/free-photo/pink-handbags_1123645.htm#fromView=search&page=2&position=3&uuid=950d5669-c190-46d8-8b15-ac76797f45ff',
+        price:50,
+        stock:false
+    },
+    {
+        id: 4,
+        title:'Chuck Taylor All Star Leater',
+        image:'https://www.freepik.com/free-photo/fashion-shoes-sneakers_1123450.htm#fromView=search&page=1&position=23&uuid=950d5669-c190-46d8-8b15-ac76797f45ff',
+        price:50,
+        stock:true
+    },
+    {
+        id: 5,
+        title:'Chuck Taylor All Star Leater',
+        image:'https://www.freepik.com/free-vector/headphones-wireless-realistic-composition-with-isolated-image-phones-with-power-bank-dock-station-with-reflections-vector-illustration_26765185.htm#fromView=search&page=1&position=1&uuid=bfe0b97e-00d1-4ebd-8f6f-1da9eedd9933',
+        price:50,
+        stock:true
+    },
+    {
+        id: 6,
+        title:'Chuck Taylor All Star Leater',
+        image:'https://www.freepik.com/free-vector/shopping-bags-mockup-realistic_6086083.htm#fromView=search&page=1&position=23&uuid=950d5669-c190-46d8-8b15-ac76797f45ff',
+        price:50,
+        stock:true
+    },
+    {
+        id: 7,
+        title:'Chuck Taylor All Star Leater',
+        image:'https://www.freepik.com/free-photo/pink-handbags_1123645.htm#fromView=search&page=2&position=3&uuid=950d5669-c190-46d8-8b15-ac76797f45ff',
+        price:50,
+        stock:false
+    },
+    {
+        id: 8,
+        title:'Chuck Taylor All Star Leater',
+        image:'https://www.freepik.com/free-photo/fashion-shoes-sneakers_1123450.htm#fromView=search&page=1&position=23&uuid=950d5669-c190-46d8-8b15-ac76797f45ff',
+        price:50,
+        stock:true
+    }
+]
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,33 +1,39 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import { dataList } from "../data/productData.js";
 
 const Home = () => {
   return (
     <Layout>
       <div className="home-main">
         <div className="first-section">
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product2.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
+          {dataList.map((e) => {
+            return (
+              <div key ={e.id} className="card">
+                <div className="card-img">
+                  <img src={e.image} alt="Product 1" />
+                </div>
+                <div className="card-detail">
+                  <div className="card-heading">
+                    <h4>{e.title}</h4>
+                  </div>
+                  <div className="card-price">
+                    <p>
+                      Price:<span className="price">$50</span>
+                    </p>
+                    <p>
+                      Stock Available:<span style={{color:e.stock?'green':"red"}} className="stock">{e.stock ? "YES":"NO"}</span>
+                    </p>
+                  </div>
+                </div>
+                <div className="cart-btn">
+                  <button>Add To Cart</button>
+                </div>
               </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-          <div className="card">
+            );
+          })}
+
+          {/* <div className="card">
             <div className="card-img">
               <img src="images/Product4.JPG" alt="Product 1" />
             </div>
@@ -91,272 +97,7 @@ const Home = () => {
             <div className="cart-btn">
               <button>Add To Cart</button>
             </div>
-          </div>
-        </div>
-
-        <div className="first-section">
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product.7JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product8.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product9.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product2.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-        </div>
-        <div className="first-section">
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product2.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product3.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product2.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product2.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-        </div>
-        <div className="first-section">
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product2.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product2.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product2.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product2.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </Layout>
