e5cf756afd32ad39e5f246b0d77cdb00ce01050a
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-03-19 10:39:35 +0500

    optimize aboutus page

--- client/src/pages/Aboutus.js
+++ client/src/pages/Aboutus.js
@@ -2,44 +2,30 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 
 const Aboutus = () => {
+  const content = `
+    At ShopSwiftly, our vision is to become the leading multi-store
+    e-commerce platform globally, known for our innovation, reliability,
+    and customer-centric approach. We envision a future where shopping
+    online is not only effortless but also enjoyable, where customers
+    have access to an extensive range of products from trusted sellers
+    at their fingertips. Through cutting-edge technology and a passion
+    for excellence.
+  `;
+
   return (
     <Layout>
       <div className="about-main">
         <div className="about-us">
-        <h3>ABOUT US</h3>
-        <p>
-        At ShopSwiftly, our vision is to become the leading multi-store
-            e-commerce platform globally, known for our innovation, reliability,
-            and customer-centric approach. We envision a future where shopping
-            online is not only effortless but also enjoyable, where customers
-            have access to an extensive range of products from trusted sellers
-            at their fingertips. Through cutting-edge technology and a passion
-            for excellence.
-        </p>
+          <h3>ABOUT US</h3>
+          <p>{content}</p>
         </div>
         <div className="mission">
           <h3>OUR MISSION</h3>
-          <p>
-          At ShopSwiftly, our vision is to become the leading multi-store
-            e-commerce platform globally, known for our innovation, reliability,
-            and customer-centric approach. We envision a future where shopping
-            online is not only effortless but also enjoyable, where customers
-            have access to an extensive range of products from trusted sellers
-            at their fingertips. Through cutting-edge technology and a passion
-            for excellence.
-          </p>
+          <p>{content}</p>
         </div>
         <div className="vission">
           <h3>OUR VISION</h3>
-          <p>
-            At ShopSwiftly, our vision is to become the leading multi-store
-            e-commerce platform globally, known for our innovation, reliability,
-            and customer-centric approach. We envision a future where shopping
-            online is not only effortless but also enjoyable, where customers
-            have access to an extensive range of products from trusted sellers
-            at their fingertips. Through cutting-edge technology and a passion
-            for excellence.
-          </p>
+          <p>{content}</p>
         </div>
       </div>
     </Layout>
