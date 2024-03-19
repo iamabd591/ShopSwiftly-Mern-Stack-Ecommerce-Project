8d9f1274dcd2480552c10a50f6e03acff84ce7a2
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-03-19 11:15:49 +0500

    optimixe code of 404

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -8,9 +8,8 @@ const PageNotFound = () => {
       <div className="d-flex align-items-center justify-content-center vh-100 notfound">
         <div className="text-center">
           <h1 className="display-1 fw-bold text-404">404</h1>
-          <p className="fs-3 p-text">
-            {" "}
-            <span className="text-danger danger">Opps!</span> Page not found.
+          <p className="fs-3 p-text text-danger">
+            Opps! Page not found.
           </p>
           <p className="lead lead-txt">
             The page you’re looking for doesn’t exist.
