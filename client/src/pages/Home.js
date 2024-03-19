613606179b39ec705c296cc9423e9c293b58b789
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-03-19 10:16:10 +0500

    optimize home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,105 +2,41 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 import { dataList } from "../data/productData.js";
 
+const Card = ({ id, image, title, price, stock }) => (
+  <div key={id} className="card">
+    <div className="card-img">
+      <img src={image} alt={title} />
+    </div>
+    <div className="card-detail">
+      <div className="card-heading">
+        <h4>{title}</h4>
+      </div>
+      <div className="card-price">
+        <p>
+          Price:<span className="price">${price}</span>
+        </p>
+        <p>
+          Stock :
+          <span className="stock" style={{ color: stock ? "green" : "red" }}>
+            {stock ? "Available" : "Not Available"}
+          </span>
+        </p>
+      </div>
+    </div>
+    <div className="cart-btn">
+      <button>Add To Cart</button>
+    </div>
+  </div>
+);
+
 const Home = () => {
   return (
     <Layout>
       <div className="home-main">
         <div className="first-section">
-          {dataList.map((e) => {
-            return (
-              <div key={e.id} className="card">
-                <div className="card-img">
-                  <img src={e.image} alt="Product 1" />
-                </div>
-                <div className="card-detail">
-                  <div className="card-heading">
-                    <h4>{e.title}</h4>
-                  </div>
-                  <div className="card-price">
-                    <p>
-                      Price:<span className="price">${e.price}</span>
-                    </p>
-                    <p>
-                      Stock :
-                      <span className="stock" style={{ color: e.stock ? "green" : "red" }}>
-                        {e.stock ? "Avaliable" : "Not Available"}
-                      </span>
-                    </p>
-                  </div>
-                </div>
-                <div className="cart-btn">
-                  <button>Add To Cart</button>
-                </div>
-              </div>
-            );
-          })}
-
-          {/* <div className="card">
-            <div className="card-img">
-              <img src="images/Product4.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product5.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card-img">
-              <img src="images/Product6.JPG" alt="Product 1" />
-            </div>
-            <div className="card-detail">
-              <div className="card-heading">
-                <h4>Chuck Taylor All Star Leater</h4>
-              </div>
-              <div className="card-price">
-                <p>
-                  Price:<span className="price">$50</span>
-                </p>
-                <p>
-                  Stock Available:<span className="stock">Yes</span>
-                </p>
-              </div>
-            </div>
-            <div className="cart-btn">
-              <button>Add To Cart</button>
-            </div>
-          </div> */}
+          {dataList.map((product) => (
+            <Card {...product} />
+          ))}
         </div>
       </div>
     </Layout>
