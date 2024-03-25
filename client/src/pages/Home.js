f34695b860874fc66055ff51fb38e6864e0c8784
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-03-25 11:37:21 +0500

    update banner in home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,27 +5,21 @@ import Card from "../components/layout/card";
 import Banner from "../components/layout/banner/index.jsx";
 
 const Home = () => {
-  // Filter out the banner and card images separately
-  const cardImages = dataList.filter((product) => product.type === "product");
-  const bannerImage = dataList.find((product) => product.type === "banner");
-  const remainingImages = dataList.filter(
-    (product) => product.type !== "banner" && product.type !== "product"
-  );
-
   return (
     <Layout>
       <div className="home-main">
         <div className="first-section">
-          {/* Render first 9 card images */}
-          {cardImages.slice(0, 9).map((product) => (
-            <Card key={product.id} {...product} />
-          ))}
-          {/* Render the banner image */}
-          {bannerImage && <Banner key={bannerImage.id} {...bannerImage} />}
-          {/* Render the rest of the images */}
-          {remainingImages.map((product) => (
-            <Card key={product.id} {...product} />
-          ))}
+          {dataList?.map((product) => {
+            return (
+              <>
+                {product.type === "product" ? (
+                  <Card key={product.id} {...product} />
+                ) : (
+                  <Banner key={product.id} {...product} />
+                )}
+              </>
+            );
+          })}
         </div>
       </div>
     </Layout>
